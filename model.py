14f5de24561fc5570feb11d5afe7e7db4b1b71c6
Mounika2405 <mounikakankanti24@gmail.com>
2020-06-05 14:42:53 +0530

    Changed activations

--- model.py
+++ model.py
@@ -4,42 +4,24 @@ import torch.nn as nn
 
 class LID(nn.Module):
     
-    def __init__(self, embedding_dim, hidden_dim, vocab_size, device):
+    def __init__(self, embedding_dim, hidden_dim, vocab_size):
         super(LID, self).__init__()
 
         self.embedding_dim = embedding_dim
         self.hidden_dim = hidden_dim
         self.vocab_size = vocab_size
-        self.device = device
         
-        self.l = nn.Linear(21, embedding_dim, bias=False)
-        #Embedding Layer
-        # self.embed = nn.Embedding(vocab_size, embedding_dim)
-        
-        #Hidden Layer
-        # self.h = nn.Linear(embedding_dim, hidden_dim)
+        self.l = nn.Linear(57, embedding_dim, bias=False)
         self.h = nn.Linear(self.embedding_dim, 2)
-        
-        #ReLU Layer
-        self.relu = nn.ReLU()
-        
-        #Softmax Layer
-        self.softmax = nn.Softmax()
+        self.tanh = nn.Tanh()
+        self.batchnorm = nn.BatchNorm1d(2)
+        self.softmax = nn.Softmax(dim=1)
 
-        
     def forward(self, x):
-        #Hidden Layer
-        
-        # embedding = self.embed(x)
-        embedding = self.l(x)
-        # print('embedding_shape',embedding.shape)
-        hidden = self.h(embedding)
-        # print('hidden_shape', hidden.shape)
-        hidden = self.relu(hidden)
-        # print('hidden after relu', hidden.shape)
-        
+
+        embedding = self.tanh(self.l(x))
+        hidden = self.tanh(self.batchnorm(self.h(embedding)))        
         out = self.softmax(hidden) #outputs probabilities for each language
-        
         return out
 
         
